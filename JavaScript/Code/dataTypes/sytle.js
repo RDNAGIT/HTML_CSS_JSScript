d9b3a58449836cd20507cff2c82bcd3560477d1b
rdnagit <ravikaush@gmail.com>
2025-03-16 20:54:41 -0400

    Message

---
+++ JavaScript/Code/dataTypes/sytle.js
@@ -0,0 +1,9 @@
+let a = 5;
+a= 5 + '5';
+a="prasad" + 2;
+console.log(a, typeof a);
+a="Ravindra";
+console.log(a, typeof a);
+a=true;
+a=1+true;
+console.log(a, typeof a );
---
+++ JavaScript/Code/first/script.js
@@ -0,0 +1,8 @@
+//this is single line comments
+/* this is multi
+lines comments
+*/
+
+console.log("My First JavaScript code")
+alert("Happy Holi")
+confirm("are you sure to delete")
